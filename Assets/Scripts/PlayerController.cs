ef07049d20a4de7805837fdc0de1c89d764983c4
Zander Goodwin <zanderngoodwin@gmail.com>
2026-03-10 18:51:13 -0300

    Player Movement works

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public Vector2 moveInput;
+
+    private Rigidbody playerRB;
+    
+    private void Awake()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        HandlePlayerMovement();
+    }
+
+    private void HandlePlayerMovement()
+    {
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y) * moveSpeed * Time.deltaTime;
+        playerRB.MovePosition(playerRB.position + move);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+}
