556a61b28d7ca164247901075719e78145ee9ee8
Zander Goodwin <zanderngoodwin@gmail.com>
2026-04-09 13:39:28 -0300

    Added a primitive button

--- Assets/Scripts/SpawnerButton.cs
+++ Assets/Scripts/SpawnerButton.cs
@@ -4,6 +4,13 @@ public class SpawnerButton : MonoBehaviour
 {
     public ObjectSpawner objectSpawner;
 
+    private GameObject buttonOffGO;
+
+    private void Start()
+    {
+        buttonOffGO = transform.GetChild(0).gameObject;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -12,11 +19,14 @@ public class SpawnerButton : MonoBehaviour
             {
                 objectSpawner.spawnerOn = true;
 
+                buttonOffGO.SetActive(false);
+
                 Instantiate(objectSpawner.objectToSpawn, objectSpawner.transform.position, Quaternion.identity);
             }
             else if (objectSpawner.spawnerOn)
             {
                 objectSpawner.spawnerOn = false;
+                buttonOffGO.SetActive(true);
             }
         }
     }
